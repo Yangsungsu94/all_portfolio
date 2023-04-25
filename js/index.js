8d672d599cd4500467fbf48dfa2cb579dafc2453
Yangungsu <119376049+Yangsungsu94@users.noreply.github.com>
2023-04-25 20:24:38 +0900

    2023-04-25

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+// scroll top0
+
+// window.onload=function(){
+//   setTimeout(function(){
+//     scrollTo(0,0);
+//   },100);
+// }
+
+// end
+
 // swiper
 var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
@@ -195,3 +205,95 @@ document.addEventListener("mousemove", e => {
 })
 //end
 
+// card
+// gsap.registerPlugin(Flip);
+
+// const activeClass = "is-active";
+// const inactiveClass = "is-inactive";
+// const cards = document.querySelectorAll(".card");
+
+// cards.forEach((card, idx) => {
+//   card.addEventListener("click", () => {
+//     const state = Flip.getState(cards);
+//     const isCardActive = card.classList.contains(activeClass);
+
+//     cards.forEach((otherCard, otherIdx) => {
+//       otherCard.classList.remove(activeClass);
+//       otherCard.classList.remove(inactiveClass);
+//       if (!isCardActive && idx !== otherIdx)
+//         otherCard.classList.add(inactiveClass);
+//     });
+
+//     if (!isCardActive) {
+//       card.classList.add(activeClass);
+//     }
+
+//     Flip.from(state, {
+//       duration: 1,
+//       ease: "expo.out",
+//       absolute: true
+//     });
+//   });
+// });
+
+// end
+
+
+// 내가 원하던거
+gsap.registerPlugin(Flip);
+
+// Variables
+const activeClass = "is-active";
+const cards = document.querySelectorAll("[data-card]");
+
+const updateCard = (card, idx, active) => {
+  const cardInner = card.querySelector(".card__inner");
+  const image = card.querySelector(".card__image");
+
+  // Bail out if we're in the middle of a flip
+  if (Flip.isFlipping(cardInner)) return;
+
+  const cardState = Flip.getState(cardInner, {
+    props: "box-shadow, border-radius"
+  });
+  const imageState = Flip.getState(image);
+  card.classList.toggle(activeClass, active);
+
+  const duration = active ? 0.7 : 0.5;
+  const ease = "quint.out";
+
+  const cardContent = document.querySelectorAll(".content__group")[idx];
+  gsap.killTweensOf(cardContent);
+  gsap.to(cardContent, {
+    duration: active ? 1 : 0.2,
+    ease: "expo.out",
+    stagger: 0.1,
+    alpha: active ? 1 : 0,
+    y: active ? 0 : 20,
+    delay: active ? 0.4 : 0
+  });
+
+  Flip.from(cardState, {
+    duration: duration,
+    ease: ease,
+    absolute: true,
+    zIndex: 1
+  });
+
+  Flip.from(imageState, {
+    duration: duration,
+    absolute: true,
+    ease: ease,
+    simple: true
+  });
+};
+
+// Init
+cards.forEach((card, idx) => {
+  updateCard(card, idx, false);
+  card.addEventListener("click", (evt) => {
+    updateCard(card, idx, !card.classList.contains(activeClass));
+  });
+});
+
+// end
